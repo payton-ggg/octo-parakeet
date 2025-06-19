b0c30a31571525ead9f28e62b330644d892e9081
Platon <platon20081019@gmail.com>
2025-06-20 00:27:41 +0300

    feat(layout): add Inter font to improve typography
    Apply Inter font to the root layout to enhance visual consistency and readability across the application

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 import { ReduxProvider } from "../store/provider";
 
 export const metadata: Metadata = {
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
