769a54151727fe649fe8990d871e345ca3a34ef3
Platon <platon20081019@gmail.com>
2025-06-22 22:02:24 +0300

    build(client): update dependencies and remove unused font
    - Add lightn

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { ReduxProvider } from "../store/provider";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
+      {/* <html lang="en" className={inter.className}> */}
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/icon.png" />
